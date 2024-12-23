4b1ff535ddd980cd9c34d2c00430f76e25cef674
NetPunk Studio <31109226+netpunkstudio@users.noreply.github.com>
2024-12-23 10:02:59 -0600

    feat: Added initial resource.

---
+++ addons/netpunk.gametools/netpunkgametoolsresource.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name NetpunkGameToolsResource
+## This resource maintains all settings made in the editor and is used to set
+## the defaults for a fresh run of the game. 
+
+## Path to folder where settings will be stored. 
+@export var basepath: String

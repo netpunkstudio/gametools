098cc9379bbb7b758429f1ec6af668a8113a4bab
NetPunk Studio <31109226+netpunkstudio@users.noreply.github.com>
2024-12-26 16:51:42 -0600

    feat: Started Implementing the Settings builder components

---
+++ addons/netpunk.gametools/subsystems/setting_builder/settings_group.gd
@@ -0,0 +1,41 @@
+@tool
+extends VBoxContainer
+
+@export var settings_key: String:
+	set(value):
+		settings_key = value
+		update_title(settings_key)
+
+@export var user_expandable: bool = true:
+	set(value):
+		user_expandable = value
+		update_addbtn(user_expandable)
+
+var setting_value = preload("res://addons/netpunk.gametools/subsystems/setting_builder/setting_value.tscn")
+
+func _ready() -> void:
+	update_title(settings_key)
+	#call_deferred("update_addbtn", user_expandable)
+
+func export_settings() -> Array:
+	var child_dict = {}
+	for child in %SettingsContents.get_children():
+		var child_data = child.export_settings()
+		if child_data[0] not in child_dict.keys():
+			child_dict[child_data[0]] = child_data[child_data[1]]
+		else: 
+			print_debug("Settings name collision")
+	return [settings_key, child_dict]
+
+func update_title(value: String) -> void:
+	print('here')
+	%Title.text = value
+
+func update_addbtn(value: bool) -> void:
+	%AddButton.visible = value
+	%AddButton.disabled = value
+
+
+func _on_add_button_pressed() -> void:
+	var new_setting = setting_value.instantiate()
+	%SettingsContents.add_child(new_setting)
---
+++ addons/netpunk.gametools/subsystems/setting_builder/settings_tab_bar.gd
@@ -0,0 +1,13 @@
+extends TabBar
+
+@export var settings_key: String
+
+func export_settings():
+	var child_dict = {}
+	for child in get_children():
+		var child_data = child.export_settings()
+		if child_data[0] not in child_dict.keys():
+			child_dict[child_data[0]] = child_data[child_data[1]]
+		else: 
+			print_debug("Settings name collision")
+	return [settings_key, child_dict]

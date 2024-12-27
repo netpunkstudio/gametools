489e440f0684c703382bb795585a1b13826a9165
NetPunk Studio <31109226+netpunkstudio@users.noreply.github.com>
2024-12-27 12:15:05 -0600

    ci: Should update tag inside the plugin to match release tag

--- release.config.js
+++ release.config.js
@@ -3,6 +3,35 @@ const config = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        [
+            "semantic-release-replace-plugin", 
+            {
+                "replacements": [
+                    {
+                        "files": ["addons/plugin.cfg"],
+                        "from": "version=\".*\"",
+                        "to": "version=\"${nextRelease.version}\"",
+                    "results": 
+                    [
+                        {
+                            "file": "foo/__init__.py",
+                            "hasChanged": true,
+                            "numMatches": 1,
+                            "numReplacements": 1,
+                        }
+                    ],
+                    "countMatches": true,
+                    }
+                ]
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": ["addons/plugin.cfg"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ],
         '@semantic-release/github'
     ],
 };

0ea0e813f9b04d5095f3b26e2defacc238762f40
NetPunk Studio <31109226+netpunkstudio@users.noreply.github.com>
2024-12-27 11:01:50 -0600

    fix (setting builder): Option entries can now be deleted.

--- addons/netpunk.gametools/subsystems/setting_builder/SettingFlags/option_button_entry.gd
+++ addons/netpunk.gametools/subsystems/setting_builder/SettingFlags/option_button_entry.gd
@@ -1,4 +1,13 @@
 extends HBoxContainer
 
+func _ready() -> void:
+	%MenuButton.get_popup().connect("id_pressed", _on_menu_item_pressed)
+
 func export_settings() -> String:
 	return %LineEdit.text
+
+func _on_menu_item_pressed(id: int) -> void:
+	var selection = %MenuButton.get_popup().get_item_text(id)
+	match selection:
+		"Delete Entry":
+			queue_free()
